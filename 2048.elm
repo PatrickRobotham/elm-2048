bb236c15ee669f59cf4f24dc3d5ae33cc5739abb
Patrick Robotham <patrick@Patricks-MacBook-Pro.local>
2014-07-19 14:13:03 +1000

    Added the ability to move right

--- 2048.elm
+++ 2048.elm
@@ -3,6 +3,8 @@ import String
 import Graphics.Input as Input
 import Text
 import Window
+import Keyboard
+
 -- Model
 
 grid : [[Int]]
@@ -36,18 +38,61 @@ border x = let w : Int
 
 box num = border (container 50 50 middle (mono num))
 
-
-displayGrid = flow down (map (flow right . map box) grid)
+displayGrid : [[Int]] -> Element
+displayGrid inpt = flow down (map (flow right . map box) inpt)
 
 
 -- Update
 
 
 
+rightCell : Int -> [Int] -> [Int]
+-- takes the (1-indexed) position of a cell in a row, and returns
+-- the result of moving that cell right.
+-- Starts from the rightmost cell.
+rightCell x ls = let width = length ls
+                     lefts = take (x-1) ls
+                     cell = head (drop (x-1) (take x ls))
+                     rights = drop x ls 
+                     spaces = filter (\x -> x == 0) rights
+                     solids = filter (\x -> x /= 0) rights
+                 in 
+                   if | x == width -> ls
+                      | isEmpty solids -> lefts ++ rights ++ [cell]
+                      | head solids == cell -> lefts ++ spaces ++ 
+                                               (0 :: cell*2 :: tail solids)
+                      | otherwise -> lefts ++ spaces ++ [cell] ++ solids
+
+rightRow : [Int] -> [Int]
+-- moves all the cells in a row to the right
+rightRow ls = foldr rightCell ls [1..length ls]
+
+moveRight : {x : Int, y : Int} -> [[Int]] -> [[Int]]
+moveRight {x} gr = if x > 0 then 
+                       map rightRow gr
+                   else gr
+
+
+{- 
+leftRow : [Int] -> [Int]
+leftRow ls = reverse (rightRow (reverse ls))
+
+moveLeft = {x} gr = if x < 0 then 
+                        map leftRow gr
+                    else gr
+
+transpose : [[a]] -> [[a]]
+transpose ls = if | isEmpty ls = []
+                  | isEmpty (head ls) = []
+                  | otherwise = map head ls : transpose (tail ls)
+
+moveUp = 
+-}
+
 
 -- 2048
 
+input = Keyboard.arrows
 
-main : Element
-main = displayGrid
+main =  (lift displayGrid) (foldp moveRight grid input) 
 
