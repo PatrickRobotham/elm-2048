855007abd1b4485442ec6cde3fc5ed5e5f78bb91
Patrick Robotham <patrick@Patricks-MacBook-Pro.local>
2014-07-19 15:50:38 +1000

    Added most rules, have to add win/lose conditions

--- 2048.elm
+++ 2048.elm
@@ -4,14 +4,11 @@ import Graphics.Input as Input
 import Text
 import Window
 import Keyboard
-
+import Random
+import Array
 -- Model
 
-grid : [[Int]]
-grid = [[2,4,8,16],
-        [2,4,8,8],
-        [4,0,2,16],
-        [0,2,8,2048]]
+state = {grid = [[2,0,0,0],[0,0,0,0],[0,0,0,0],[0,0,0,0]], turn = True}
 
 -- Display
 
@@ -38,8 +35,7 @@ border x = let w : Int
 
 box num = border (container 50 50 middle (mono num))
 
-displayGrid : [[Int]] -> Element
-displayGrid inpt = flow down (map (flow right . map box) inpt)
+displayGrid inpt = flow down (map (flow right . map box) inpt.grid)
 
 
 -- Update
@@ -67,37 +63,56 @@ rightRow : [Int] -> [Int]
 -- moves all the cells in a row to the right
 rightRow ls = foldr rightCell ls [1..length ls]
 
-moveRight : {x : Int, y : Int} -> [[Int]] -> [[Int]]
-moveRight {x} gr = if x > 0 then 
-                       map rightRow gr
-                   else gr
-
-
-
+moveRight :  [[Int]] -> [[Int]]
+moveRight gr = map rightRow gr
+               
 leftRow : [Int] -> [Int]
 leftRow ls = reverse (rightRow (reverse ls))
 
-moveLeft {x} gr = if x < 0 then 
-                        map leftRow gr
-                    else gr
-
-{-
-
+moveLeft gr = map leftRow gr
+                    
 transpose : [[a]] -> [[a]]
 transpose ls = if | isEmpty ls -> []
                   | isEmpty (head ls) -> []
                   | otherwise -> map head ls :: transpose (map tail ls)
 
+moveUp gr = transpose (moveLeft (transpose gr))
 
-moveUp = 
--}
+moveDown gr = transpose (moveRight (transpose gr))
+                
+index n xs = if | n == 0 -> head xs
+           | otherwise -> index (n-1) (tail xs)
 
-play inpt = moveLeft inpt . moveRight inpt
+lookup : (Int , Int) -> [[a]] -> a
+lookup (x,y) ls = index y (index x ls) 
 
+coords = (concatMap (\x -> zip (repeat 4 x) [0..3]) [0..3])
+
+place : Int -> [[Int]] -> [[Int]]
+-- Assumes grid has at least one empty space
+place seed gr = let empty (x,y) = lookup (x,y) gr == 0
+                    spaces = filter empty coords
+                    (i,j) = index (mod seed (length spaces)) spaces
+                    ar = Array.fromList (map Array.fromList gr)
+                    updatedRow = Array.set j 2 (Array.getOrFail i ar)
+                in 
+                  Array.toList (Array.map Array.toList (Array.set i updatedRow ar))
+      
+
+play ({x,y},n) gr = 
+    if | gr.turn -> {gr | grid <- place n gr.grid, turn <- False}
+       | x > 0 -> {gr | grid <- moveRight gr.grid,turn <-  moveRight gr.grid /= gr.grid}
+       | x < 0 -> {gr | grid <- moveLeft gr.grid, turn <- moveLeft gr.grid /= gr.grid}
+       | y > 0 -> {gr | grid <- moveUp gr.grid, turn <- moveUp gr.grid /= gr.grid}
+       | y < 0 -> {gr | grid <- moveDown gr.grid, turn <- moveDown gr.grid /= gr.grid}
+       | otherwise -> gr
+                       
 
 -- 2048
 
-input = Keyboard.arrows
+keys = Keyboard.arrows
 
-main =  (lift displayGrid) (foldp play grid input) 
+input = lift2 (,) keys (Random.range 0 16 keys)
+
+main =  (lift displayGrid) (foldp play state input)
 
